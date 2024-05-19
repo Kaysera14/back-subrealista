87037c7acd8cd940e7b3b01ad8994f3980e66713
Kaysera14 <131011220+Kaysera14@users.noreply.github.com>
2024-05-19 13:58:10 +0200

    first iteration of the changes

--- db/populateDB.js
+++ db/populateDB.js
@@ -14,8 +14,8 @@ const populate = async () => {
         INSERT INTO users (username, email, password, active)
         VALUES 
         ('Manu','konstantinoff080696@gmail.com','${hashedPass}', 1),
-        ('Oscar','negociominimoviable@gmail.com','${hashedPass}', 1),
-        ('Yese','yese.astarloa@email.com','${hashedPass}', 1),
+        ('Oscar','oscar@email.com','${hashedPass}', 1),
+        ('Yese','yese.astarloa@gmail.com','${hashedPass}', 1),
         ('Toni','antoniorondanvlc@gmail.com','${hashedPass}', 1),
         ('Paco','paco@email.com','${hashedPass}', 1),
         ('Elena','elena@email.com','${hashedPass}', 1),
